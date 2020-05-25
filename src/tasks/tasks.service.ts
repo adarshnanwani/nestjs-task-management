c02f6fab651c9bb795651c0cfc9ad0441333c1c9
Adarsh Nanwani <adarsh.nanwani@sapiens.com>
2020-05-25 18:00:42 +0530

    Add custom error message.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -29,7 +29,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const task = this.tasks.find(task => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    } else {
+      return task;
+    }
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
