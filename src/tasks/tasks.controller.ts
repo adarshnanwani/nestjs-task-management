fbce514f6f96f87148754a23ea03ffc0c57bade1
Adarsh Nanwani <adarsh.nanwani@sapiens.com>
2020-05-25 17:05:57 +0530

    Add getTaskById by using @Param() decorator.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -13,9 +13,16 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     const task = this.tasksService.createTask(createTaskDto);
     return task;
   }
+
 }
